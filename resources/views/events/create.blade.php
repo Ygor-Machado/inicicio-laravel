178acbc46d35f9e09a5f9cb487b0c7cc4bbc3d7d
Ygor Sousa Machado <yggor@musixe.com>
2023-07-05 00:54:46 -0300

    salvando json no banco de dados

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -36,6 +36,26 @@
             <textarea name="description" id="description" class="form-control" placeholder="Descrição do evento"></textarea>
         </div>
 
+        <div class="form-group">
+            <label for="title">Adicione itens de infraestrutura:</label>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Palco"> Palco
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cerveja Grátis"> Cerveja Grátis
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Open Food"> Open Food
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Brindes"> Brindes
+            </div>
+        </div>
+
         <input type="submit" class="btn btn-primary" value="Criar Evento">
     </form>
 </div>
