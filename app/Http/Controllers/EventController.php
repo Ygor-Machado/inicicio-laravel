80fa488628d679c3bdfe680d3f9a66db0f6da540
Ygor Sousa Machado <yggor@musixe.com>
2023-07-06 01:40:41 -0300

    Relação entre tabelas

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -78,6 +78,9 @@ class EventController extends Controller
 
         }
 
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento Criado com sucesso!');
