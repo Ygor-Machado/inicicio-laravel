6f52a5d9107a4fa09476613f960e8422c8766552
Ygor Sousa Machado <yggor@musixe.com>
2023-07-15 00:59:25 -0300

    organiza css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/styles.css">
+        <link rel="stylesheet" href="/styles/styles.css">
         <script src="/js/script.js"></script>
 
 
